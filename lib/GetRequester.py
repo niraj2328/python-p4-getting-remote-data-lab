9e1bd855004e27e1abad2b6e16d8acc545d6d3a3
Niraj <niraj.patel@flatironschool.com>
2023-02-27 06:31:13 -0500

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,10 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
-
-    def load_json(self):
-        pass
+    def get_json(self):
+        response = requests.get(self.url)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx errors
+        return response.json()
